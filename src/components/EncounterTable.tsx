ca1ea715a30844bdff87234393ec0b3e45185920
saucefoong <sheeyao@hotmail.com>
2022-10-07 12:13:34 +0800

    Fixed Table Index Error

--- src/components/EncounterTable.tsx
+++ src/components/EncounterTable.tsx
@@ -217,155 +217,154 @@ function Table({ data, columns }: { data: any; columns: ColumnDef<any>[] }) {
 
   return (
     <Box padding={"20px"}>
-      
       <Card title={"Encounters Table"}>
         <Box overflowX="scroll">
-        <ChakraTable colorScheme="blue" size="md">
-          <Thead>
-            {table.getHeaderGroups().map((headerGroup: any) => (
-              <Tr key={headerGroup.id}>
-                <Th>#</Th>
+          <ChakraTable colorScheme="blue" size="md">
+            <Thead>
+              {table.getHeaderGroups().map((headerGroup: any) => (
+                <Tr key={headerGroup.id}>
+                  <Th>#</Th>
 
-                {headerGroup.headers.map((header: any) => (
-                  <Th key={header.id} colSpan={header.colSpan}>
-                    {header.id === "select" &&
-                      flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                    {!header.isPlaceholder && header.id !== "select" && (
-                      <Box
-                        onClick={header.column.getToggleSortingHandler()}
-                        cursor="pointer"
-                      >
-                        <Stack
-                          alignItems="center"
-                          direction={"row"}
-                          spacing={2}
+                  {headerGroup.headers.map((header: any) => (
+                    <Th key={header.id} colSpan={header.colSpan}>
+                      {header.id === "select" &&
+                        flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      {!header.isPlaceholder && header.id !== "select" && (
+                        <Box
+                          onClick={header.column.getToggleSortingHandler()}
+                          cursor="pointer"
                         >
-                          {flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                          {
+                          <Stack
+                            alignItems="center"
+                            direction={"row"}
+                            spacing={2}
+                          >
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
                             {
-                              asc: <TriangleUpIcon />,
-                              desc: <TriangleDownIcon />,
-                              false: <MinusIcon />,
-                            }[`${header.column.getIsSorted()}` as string]
-                          }
-                        </Stack>
-                      </Box>
-                    )}
-                  </Th>
-                ))}
-              </Tr>
-            ))}
-          </Thead>
+                              {
+                                asc: <TriangleUpIcon />,
+                                desc: <TriangleDownIcon />,
+                                false: <MinusIcon />,
+                              }[`${header.column.getIsSorted()}` as string]
+                            }
+                          </Stack>
+                        </Box>
+                      )}
+                    </Th>
+                  ))}
+                </Tr>
+              ))}
+            </Thead>
 
-          <Tbody>
-            {loading ? (
-              <TableLoading columnsToLoad={5} rowsToLoad={10} />
-            ) : (
-              <>
-                {table.getRowModel().rows.map((row: any, index: number) => (
-                  // <Link as={RouterLink} to={`patient/${data[index]}`}>
-                  <Tr
-                    key={row.id}
-                    onClick={() => onRowClick(index, data)}
-                    _hover={{
-                      cursor: "pointer",
-                      bg: "blue.200",
-                      rounded: "md",
-                    }}
-                  >
-                    <Td key={index}>{index + 1}</Td>
-                    {row.getVisibleCells().map((cell: any) => (
-                      <>
-                        <Td key={cell.id}>
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext()
-                          )}
-                        </Td>
-                      </>
-                    ))}
-                  </Tr>
-                  // </Link>
-                ))}
-              </>
-            )}
-          </Tbody>
-        </ChakraTable>
+            <Tbody>
+              {loading ? (
+                <TableLoading columnsToLoad={5} rowsToLoad={10} />
+              ) : (
+                <>
+                  {table.getRowModel().rows.map((row: any, index: number) => (
+                    // <Link as={RouterLink} to={`patient/${data[index]}`}>
+                    <Tr
+                      key={row.id}
+                      onClick={() => onRowClick(row.index, data)}
+                      _hover={{
+                        cursor: "pointer",
+                        bg: "blue.200",
+                        rounded: "md",
+                      }}
+                    >
+                      <Td key={index}>{row.index + 1}</Td>
+                      {row.getVisibleCells().map((cell: any) => (
+                        <>
+                          <Td key={cell.id}>
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}
+                          </Td>
+                        </>
+                      ))}
+                    </Tr>
+                    // </Link>
+                  ))}
+                </>
+              )}
+            </Tbody>
+          </ChakraTable>
         </Box>
       </Card>
       <br />
       <br />
 
       <div className="h-2" />
-      
-      <Center className="flex items-center gap-2">
-      <Box overflowX="scroll">
-        <HStack>
-          <IconButton
-            colorScheme="blue"
-            aria-label="Most Previous Page"
-            icon={<ArrowLeftIcon w={"2"} h={"2"} />}
-            onClick={() => table.setPageIndex(0)}
-            disabled={!table.getCanPreviousPage()}
-            size="md"
-          />
-          <IconButton
-            colorScheme="blue"
-            aria-label="Previous Page"
-            icon={<ChevronLeftIcon />}
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          />
-          <IconButton
-            colorScheme="blue"
-            aria-label="Next Page"
-            icon={<ChevronRightIcon />}
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          />
-          <IconButton
-            colorScheme="blue"
-            aria-label="Most Previous Page"
-            icon={<ArrowRightIcon w={"2"} h={"2"} />}
-            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-            disabled={!table.getCanNextPage()}
-            size="md"
-          />
 
-          <span className="flex items-center gap-1">
-            <div>Page</div>
-            <strong>
-              {table.getState().pagination.pageIndex + 1} of{" "}
-              {table.getPageCount()}
-            </strong>
-          </span>
-          <span className="flex items-center gap-1">
-            | Go to page:
-            <NumberInput
-              size="sm"
-              maxW={16}
-              defaultValue={table.getState().pagination.pageIndex + 1}
-              value={table.getState().pagination.pageIndex + 1}
-              min={1}
-              max={table.getPageCount()}
-              onChange={(val: any) => {
-                const page = val ? val - 1 : 0;
-                table.setPageIndex(page);
-              }}
-            >
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-            {/* <input
+      <Center className="flex items-center gap-2">
+        <Box overflowX="scroll">
+          <HStack>
+            <IconButton
+              colorScheme="blue"
+              aria-label="Most Previous Page"
+              icon={<ArrowLeftIcon w={"2"} h={"2"} />}
+              onClick={() => table.setPageIndex(0)}
+              disabled={!table.getCanPreviousPage()}
+              size="md"
+            />
+            <IconButton
+              colorScheme="blue"
+              aria-label="Previous Page"
+              icon={<ChevronLeftIcon />}
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            />
+            <IconButton
+              colorScheme="blue"
+              aria-label="Next Page"
+              icon={<ChevronRightIcon />}
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            />
+            <IconButton
+              colorScheme="blue"
+              aria-label="Most Previous Page"
+              icon={<ArrowRightIcon w={"2"} h={"2"} />}
+              onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+              disabled={!table.getCanNextPage()}
+              size="md"
+            />
+
+            <span className="flex items-center gap-1">
+              <div>Page</div>
+              <strong>
+                {table.getState().pagination.pageIndex + 1} of{" "}
+                {table.getPageCount()}
+              </strong>
+            </span>
+            <span className="flex items-center gap-1">
+              | Go to page:
+              <NumberInput
+                size="sm"
+                maxW={16}
+                defaultValue={table.getState().pagination.pageIndex + 1}
+                value={table.getState().pagination.pageIndex + 1}
+                min={1}
+                max={table.getPageCount()}
+                onChange={(val: any) => {
+                  const page = val ? val - 1 : 0;
+                  table.setPageIndex(page);
+                }}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              {/* <input
                 type="number"
                 defaultValue={table.getState().pagination.pageIndex + 1}
                 onChange={(e) => {
@@ -374,25 +373,24 @@ function Table({ data, columns }: { data: any; columns: ColumnDef<any>[] }) {
                 }}
                 className="border p-1 rounded w-16"
               /> */}
-          </span>
-          <select
-            value={table.getState().pagination.pageSize}
-            onChange={(e) => {
-              table.setPageSize(Number(e.target.value));
-            }}
-          >
-            {[10, 20, 30, 40, 50].map((pageSize) => (
-              <option key={pageSize} value={pageSize}>
-                Show {pageSize}
-              </option>
-            ))}
-          </select>
+            </span>
+            <select
+              value={table.getState().pagination.pageSize}
+              onChange={(e) => {
+                table.setPageSize(Number(e.target.value));
+              }}
+            >
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+                </option>
+              ))}
+            </select>
 
-          {/* {JSON.stringify(patients)} */}
-        </HStack>
+            {/* {JSON.stringify(patients)} */}
+          </HStack>
         </Box>
       </Center>
-      
     </Box>
   );
 }
